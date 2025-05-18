1768dc697da5ac1cc85e54d7933b5d1123e37f91
Ramanan <211810143+ramanan-1@users.noreply.github.com>
2025-05-18 19:17:59 +0530

    RansomNote

--- hashmap/ransomnote.py
+++ hashmap/ransomnote.py
@@ -1,1 +1,24 @@
-# iwill do it at 7:15
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+
+        freq = {}
+
+        #Count charFreq of char in magazine
+
+        for char in magazine :
+            if char not in freq :
+                freq[char] = 1
+            else:
+                freq[char]+=1
+
+        #loop through each element in ransomeNote
+
+        for char in ransomNote:
+
+            #if char in freq then reduce freq of char by 1
+            if char in freq and freq[char] != 0 :
+                freq[char] -= 1 
+            else:
+                return False
+        return True                       
+        
